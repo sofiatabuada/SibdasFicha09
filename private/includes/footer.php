01d371ffdb88c8c6ee398830c6c43d03c35ca80a
Sofia Tabuada <tabuadasofia@email.com>
2026-03-20 19:16:15 +0000

    organizar index

---
+++ private/includes/footer.php
@@ -0,0 +1,6 @@
+    <!-- Bootstrap JS and custom JS -->
+    <script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
+
+    </body>
+
+    </html>
---
+++ private/includes/sidebar.php
@@ -0,0 +1,26 @@
+<!-- Sidebar -->
+<aside class="col-md-3 col-lg-2 bg-secondary text-white p-3 min-vh-100">
+    <h4>Menu</h4>
+    <nav>
+        <a href="views/clientes/lista.html" class="nav-link text-white px-0 mb-2 d-block">
+            <i class="fas fa-users"></i> &ensp; Clientes
+        </a>
+
+        <a href="views/agendamento/agendamento.html" class="nav-link text-white px-0 mb-2 d-block">
+            <i class="fas fa-calendar-alt"></i> &ensp; Agendamento de treinos
+        </a>
+
+        <a href="views/plano_treino/plano_treino.html" class="nav-link text-white px-0 mb-2 d-block">
+            <i class="fas fa-dumbbell"></i> &ensp; Planos de Treino
+        </a>
+
+        <a href="views/equipamentos/equipamentos.html" class="nav-link text-white px-0 mb-2 d-block">
+            <i class="fas fa-cogs"></i> &ensp; Equipamentos
+        </a>
+
+        <a href="views/produtos_servicos/produtos_servicos.html"
+            class="nav-link text-white px-0 mb-2 d-block">
+            <i class="fas fa-box-open"></i> &ensp; Produtos e Serviços
+        </a>
+    </nav>
+</aside>
